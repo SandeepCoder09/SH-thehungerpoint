fefa416c982eb5bc2f2dfa77aae6e22c65bf20fc
Sandeep Gautam <161199543+SandeepCoder09@users.noreply.github.com>
2025-11-14 12:28:40 +0530

    Update Firebase admin config to use environment variables

--- server/razorpay-server.js
+++ server/razorpay-server.js
@@ -26,18 +26,19 @@ app.use(bodyParser.json());
 /* --------------------------------------
    FIREBASE ADMIN INIT USING ENV VARIABLES
 --------------------------------------- */
+// FIREBASE ADMIN CONFIG USING ENV VARIABLES
 const serviceAccount = {
-  type: process.env.service_account,
-  project_id: process.env.sh-the-hunger-point,
-  private_key_id: process.env.a0433d7793ce1127041ab022d9f5a0c3f8648f81,
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
   private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  client_email: process.env.firebase-adminsdk-fbsvc@sh-the-hunger-point.iam.gserviceaccount.com,
-  client_id: process.env.113069436511916017152,
-  auth_uri: process.env.https://oauth2.googleapis.com/token,
-  token_uri: process.env.https://www.googleapis.com/oauth2/v1/certs,
-  auth_provider_x509_cert_url: process.env.https://www.googleapis.com/oauth2/v1/certs,
-  client_x509_cert_url: process.env.https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc@sh-the-hunger-point.iam.gserviceaccount.com,
-  universe_domain: process.env.googleapis.com,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
 };
 
 admin.initializeApp({
