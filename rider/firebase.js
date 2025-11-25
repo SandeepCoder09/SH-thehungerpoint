9f804bc673ab78b643fae00b276e34b3bd2f3f20
Sandeep Gautam <161199543+SandeepCoder09@users.noreply.github.com>
2025-11-25 20:41:32 +0530

    Create firebase.js

---
+++ rider/firebase.js
@@ -0,0 +1,16 @@
+// /rider/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAyBMrrpmW0b7vhBCgaAObL0AOGeNrga_8",
+  authDomain: "sh-the-hunger-point.firebaseapp.com",
+  projectId: "sh-the-hunger-point",
+  storageBucket: "sh-the-hunger-point.firebasestorage.app",
+  messagingSenderId: "401237282420",
+  appId: "1:401237282420:web:5162604a4bb2b9799b8b21",
+  measurementId: "G-4KP3RJ15E9"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
