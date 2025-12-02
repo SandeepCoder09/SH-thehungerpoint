b20836c724b395adbdd4bfbcd4c537b84418d219
Sandeep Gautam <161199543+SandeepCoder09@users.noreply.github.com>
2025-12-02 13:17:14 +0530

    Update firebase-config.js

--- auth/firebase-config.js
+++ auth/firebase-config.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAyBMrrpmW0b7vhBCgaAObL0AOGeNrga_8",
   authDomain: "sh-the-hunger-point.firebaseapp.com",
   projectId: "sh-the-hunger-point",
-  storageBucket: "sh-the-hunger-point.firebasestorage.app",
+  storageBucket: "sh-the-hunger-point.appspot.com",
   messagingSenderId: "401237282420",
   appId: "1:401237282420:web:5162604a4bb2b9799b8b21",
   measurementId: "G-4KP3RJ15E9"
